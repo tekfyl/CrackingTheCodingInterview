26e9d216a29c99e5c7134ebae5d3371f0eaa6d7b
tekfyl <f2014435@pilani.bits-pilani.ac.in>
2017-07-11 00:15:50 +0530

    AC

---
+++ ArrayAndStrings/1.3/main2.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define all(c) c.begin(), c.end()
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector< vi >
+#define vvl vector< vector<ll> >
+#define mk make_pair
+#define ii pair <int, int>
+#define LL pair <ll, ll>
+#define fi first
+#define se second
+#define mx max_element
+#define mn min_element
+#define rep(i,n)    for(__typeof(n) i = 0; i < n; i++)
+#define rep1(i,n)   for(__typeof(n) i = 1; i <= n; i++)
+double tick(){static clock_t oldt,newt=clock();double diff=100.0*(newt-oldt)/CLOCKS_PER_SEC;oldt=newt;return diff;}
+
+using namespace std;
+
+ll inf=1e18+1;  
+ll N; 
+vll v,q,st;
+ll in=0,i=0,j=0,k=0,ans=0;
+ll n=0,m=0,t=0,v1=0,v2=0;
+char c; string s;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin >> s;
+    ll check = 0;
+    for(i=0; i<s.size(); i++){
+        int v = (int)(s[i]-'a');
+        if((check & 1<<v) == 0){
+            s[t++] = s[i];
+            check |= (1<<v);
+        }
+    }
+    for(i=0; i<t; i++) cout << s[i];
+    //cout<<"\n"<<"Execution time : "<<tick()<<"\n";
+    return 0;
+}
+
